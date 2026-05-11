4c18c58f14b8da513bfc2e56caedba403449faf5
mariaamjad <mariaamjad198@gmail.com>
2026-05-11 19:06:07 +0500

    Added EJS dynamic product rendering

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-
-const express = require('express');
+const express = require("express");
 // helps working with files and directories
-const path = require('path');
+const path = require("path");
 require("dotenv").config();
-
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const Product = require("./models/Product");
 
 const app = express();
 
 connectDB();
 
+app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'gridview.html'));
-})
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
 
-app.get('/products/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'detail.html'));
-})
+    res.render("gridview", {
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
 
+  // res.sendFile(path.join(__dirname, 'public', 'gridview.html'));
+});
+
+app.get("/products/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "detail.html"));
+});
 
 app.listen(3000, () => {
-    console.log('Connected Succesfully on port 3000.')
-})
+  console.log("Connected Succesfully on port 3000.");
+});
--- models/Product.js
+++ models/Product.js
@@ -1,29 +1,57 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  stock: {
-    type: Number,
-    default: 0,
-  },
-}, { timestamps: true });
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
+    price: {
+      type: Number,
+      required: true,
+    },
 
-module.exports = mongoose.model('Product', productSchema);
+    oldPrice: {
+      type: Number,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      default: 0,
+    },
+
+    rating: {
+      type: Number,
+      default: 0,
+    },
+
+    reviews: {
+      type: Number,
+      default: 0,
+    },
+
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
