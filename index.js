a598e7abd4f346b8f4f98c6bf9ec121856f12e89
mariaamjad <mariaamjad198@gmail.com>
2026-05-13 16:14:42 +0500

    Dynamic data randering for all pages

--- index.js
+++ index.js
@@ -13,27 +13,63 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", async(req, res) => {
+  try {
+    const products = await Product.find({featured: true}).limit(10);
+
+    res.render("index", {products,});
+  }
+  catch (error){
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+
 });
 
 app.get("/products", async (req, res) => {
   try {
-    const products = await Product.find();
+    const category = req.query.category;
 
-    res.render("gridview", {
+    let products;
+
+    if (category) {
+      products = await Product.find({category: category});
+    }
+    else{
+      products = await Product.find();
+    }
+
+    res.render("products", {
       products,
     });
-  } catch (error) {
+  } 
+  catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
   }
-
-  // res.sendFile(path.join(__dirname, 'public', 'gridview.html'));
 });
 
-app.get("/products/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "detail.html"));
+// app.get(["/listview", "/gridview"], (req, res) => {
+//   res.redirect("/products");
+// });
+
+
+app.get("/products/:id", async (req, res) => {
+  try{
+    const product = await Product.findById(req.params.id);
+
+    res.render("detail", {product,});
+  }
+  catch(error) {
+    res.status(500).send('Server Error');
+  }
+
+  // res.sendFile(path.join(__dirname, "public", "detail.html"));
+});
+
+
+app.get("/cart", (req, res) => {
+  res.render("cart");
 });
 
 app.listen(3000, () => {
